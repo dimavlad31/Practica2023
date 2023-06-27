ff0e16a041814e0cb947a459e9eb72f0b5f2c311
dimavlad31 <126650922+dimavlad31@users.noreply.github.com>
2023-06-27 10:23:52 +0300

    Add files via upload

--- analizaRetea.sh
+++ analizaRetea.sh
@@ -2,10 +2,14 @@
 
 interfata=$(sudo ip route | grep default | sed -e 's/^.*dev.//' -e 's/.proto.*//')
 
-sudo tshark -i $interfata -a duration:60 | while read -r line
+sudo tshark -i $interfata -a duration:60 | while read line
 do
 	echo $line | egrep -o "([0-9]?[0-9][0-9]?\.){3}[0-9]?[0-9][0-9]?" >> logAdreseIP.txt
-	echo $line | egrep "DNS" >> logDNS.txt
+	DNS=$(echo $line | tr -s " " | cut -d' ' -f6)
+	if [[ $DNS == "DNS" ]] 
+	then
+		echo $line >> logDNS.txt
+	fi
 	echo $line >> logTrafic.txt
 done
 
