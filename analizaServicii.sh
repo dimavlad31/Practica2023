9e6e32abaa0cb95459235430fbda3d0fcf4b102f
dimavlad31 <126650922+dimavlad31@users.noreply.github.com>
2023-06-27 10:06:53 +0300

    Add files via upload

--- analizaServicii.sh
+++ analizaServicii.sh
@@ -3,10 +3,25 @@
 
 users=$(users)
 
+serviciiActive=$(sudo systemctl list-units -t service --state=active | tail -n +2 | head -n -5 | cut -d' ' -f3 | tr "\n" " ")
+serviciiInactive=$(sudo systemctl list-units -t service --state=inactive | tail -n +2 | head -n -5 | grep "loaded" | cut -d' ' -f3 | tr "\n" " ")
+
 for i in $users
 do
-	echo Serviciile pornite ale utilizatorului $i: >> logServicii.txt
-	sudo -u $i systemctl -t service --state=running >> logServicii.txt
-	echo Serviciile oprite ale utilizatorului $i: >> logServicii.txt
-	sudo -u $i systemctl -t service --state=inactive >> logServicii.txt
+	for j in $serviciiActive
+	do
+		var=$(sudo systemctl status $j | egrep -o -m 1 $i)
+		if [[ $var == $i ]]
+		then
+			echo Utilizatorul $i are pornit serviciul $j >> logServicii.txt
+		fi
+	done
+	for j in $serviciiInactive
+	do
+		var=$(sudo systemctl status $j | egrep -o -m 1 $i)
+		if [[ $var == $i ]]
+		then
+			echo Utilizatorul $i are oprit serviciul $j >> logServicii.txt
+		fi
+	done
 done
