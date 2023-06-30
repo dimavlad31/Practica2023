a9ba8246e6424089cd91ebad72d8affa95946a26
dimavlad31 <126650922+dimavlad31@users.noreply.github.com>
2023-06-30 09:12:11 +0300

    Add files via upload

--- transmitereDate.py
+++ transmitereDate.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+import os
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4455
@@ -13,66 +14,69 @@ SIZE = 1024
 def main():   
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
-    file1=open("logActivitati.txt","r")
-    file2=open("logTaste.txt","r")
-    file3=open("istoricComenzi.txt","r")
-    file4=open("logServicii.txt","r")
-    file5=open("logDNS.txt","r")
-    file6=open("logAdreseIP.txt","r")
-    file7=open("logTrafic.txt","r")
+    if os.path.exists("logActivitati.txt"):
+        file1=open("logActivitati.txt","r")
+        for data in file1:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)    
+            data="1_"+data
+            client.send(data.encode(FORMAT))
+        file1.close()
 
-    for data in file1:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)    
-        data="1_"+data
-        client.send(data.encode(FORMAT))
+    if os.path.exists("logTaste.txt"):
+        file2=open("logTaste.txt","r")
+        for data in file2:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)    
+            data="2_"+data
+            client.send(data.encode(FORMAT))
+        file2.close()
 
-    for data in file2:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)    
-        data="2_"+data
-        client.send(data.encode(FORMAT))
+    if os.path.exists("istoricComenzi.txt"):
+        file3=open("istoricComenzi.txt","r")
+        for data in file3:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)    
+            data="3_"+data
+            client.send(data.encode(FORMAT))
+        file3.close()
 
-    for data in file3:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)    
-        data="3_"+data
-        client.send(data.encode(FORMAT))
+    if os.path.exists("logServicii.txt"):
+        file4=open("logServicii.txt","r")
+        for data in file4:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)    
+            data="4_"+data
+            client.send(data.encode(FORMAT))
+        file4.close()
 
-    for data in file4:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)    
-        data="4_"+data
-        client.send(data.encode(FORMAT))
+    if os.path.exists("logDNS.txt"):
+        file5=open("logDNS.txt","r")
+        for data in file5:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)    
+            data="5_"+data
+            client.send(data.encode(FORMAT))
+        file5.close()
 
-    for data in file5:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)    
-        data="5_"+data
-        client.send(data.encode(FORMAT))
-    
-    for data in file6:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)    
-        data="6_"+data
-        client.send(data.encode(FORMAT))
+    if os.path.exists("logAdreseIP.txt"):
+        file6=open("logAdreseIP.txt","r")
+        for data in file6:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)    
+            data="6_"+data
+            client.send(data.encode(FORMAT))
+        file6.close()
 
-    for data in file7:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)    
-        data="7_"+data
-        client.send(data.encode(FORMAT))
-
-    file5.close()
-    file6.close()
-    file7.close()
-    file4.close()
-    file3.close()
-    file1.close()
-    file2.close()
-    curata1=open("logTrafic.txt", "w")
-    curata1.write("")
-    curata1.close()
+    if os.path.exists("logTrafic.txt"):
+        file7=open("logTrafic.txt","r")
+        for data in file7:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)    
+            data="7_"+data
+            client.send(data.encode(FORMAT))
+        file7.close()
+        
     client.close()
 
 if __name__ == "__main__":
