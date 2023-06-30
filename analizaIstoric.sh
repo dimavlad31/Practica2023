45dee75b5b60799e27039c32a9ba51052c03c63c
dimavlad31 <126650922+dimavlad31@users.noreply.github.com>
2023-06-30 08:20:12 +0300

    Add files via upload

--- analizaIstoric.sh
+++ analizaIstoric.sh
@@ -5,7 +5,7 @@ users=$(users)
 
 for i in $users
 do
-	echo Comenzile user-ului $i: > istoricComenzi.txt
+	echo Comenzile user-ului $i: >> istoricComenzi.txt
 	homeDir=$(sudo cat /etc/passwd | cut -d: -f1,6 | egrep "$i:" | cut -d: -f2)
-	sudo cat $homeDir/.bash_history > istoricComenzi.txt
+	sudo cat $homeDir/.bash_history >> istoricComenzi.txt
 done
